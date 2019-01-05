4e9e1c110d8199c10f97c27f2ead21d4215dc3d8
Carroll Vance <vancecs@gmail.com>
2019-01-05 17:20:28 -0600

    Revert to old parallel_preprocessor until we can get mpi working

--- parallel/parallel_preprocessor.py
+++ parallel/parallel_preprocessor.py
@@ -179,39 +179,135 @@ class WorkerManager(object):
             sleep(0.1)
 
 
+def transform(df: pd.DataFrame, year: int, masknan: float = None) -> pd.DataFrame:
+    if masknan is None:
+
+        if year < 2014:
+            df = df[df['nox_flag'] == 'VAL']
+            df = df[df['no_flag'] == 'VAL']
+            df = df[df['no2_flag'] == 'VAL']
+            df = df[df['o3_flag'] == "VAL"]
+            df = df[df['temp_flag'] == "VAL"]
+        if year >= 2014:
+            df = df[df['nox_flag'] == 'K']
+            df = df[df['no_flag'] == 'K']
+            df = df[df['o3_flag'] == 'K']
+            df = df[df['temp_flag'] == 'K']
+
+        df = df[~df['winddir'].isna()]
+        df = df[~df['AQS_Code'].isna()]
+
+        df = df.drop(
+            ['co_flag', 'humid', 'humid_flag', 'pm25', 'pm25_flag', 'so2', 'so2_flag', 'solar', 'solar_flag', 'dew',
+             'dew_flag', 'redraw', 'co', 'no_flag', 'no2_flag', 'nox_flag', 'o3_flag', 'winddir_flag', 'windspd_flag',
+             'temp_flag', 'Longitude', 'Latitude'], axis=1)
+
+        df = df.dropna()
+
+    df['wind_x_dir'] = df['windspd'] * np.cos(df['winddir'] * (np.pi / 180))
+    df['wind_y_dir'] = df['windspd'] * np.sin(df['winddir'] * (np.pi / 180))
+    df['hour'] = pd.to_datetime(df['epoch'], unit='s').dt.hour
+    df['day_of_year'] = pd.Series(pd.to_datetime(df['epoch'], unit='s'))
+    df['day_of_year'] = df['day_of_year'].dt.dayofyear
+
+    if masknan is not None:
+        df = df.drop(
+            ['co_flag', 'humid', 'humid_flag', 'pm25', 'pm25_flag', 'so2', 'so2_flag', 'solar', 'solar_flag', 'dew',
+             'dew_flag', 'redraw', 'co', 'no_flag', 'no2_flag', 'nox_flag', 'o3_flag', 'winddir_flag', 'windspd_flag',
+             'temp_flag', 'Longitude', 'Latitude'], axis=1)
+
+        df[df.isna()] = np.nan
+
+    return df
+
+
+def transform_file(year: int, masknan: bool, input_path: str, cache_path: str):
+    df = pd.read_csv(input_path)
+    df = transform(df, year, masknan)
+    df.to_csv(cache_path)
+
+    return True
+
+
 @plac.annotations(
-    input_path=("Path containing the data files to ingest", "option", "P", str),
-    input_prefix=("{$prefix}year.csv", "option", "p", str),
-    input_suffix=("year{$suffix}.csv", "option", "s", str),
+    ingest_path=("Path containing the data files to ingest", "option", "P", str),
+    ingest_prefix=("{$prefix}year.csv", "option", "p", str),
+    ingest_suffix=("year{$suffix}.csv", "option", "s", str),
     output_path=("Path to write the resulting numpy sequences / transform cache", "option", "o", str),
     year_begin=("First year to process", "option", "b", int),
     year_end=("Year to stop with", "option", "e", int),
     num_jobs=("Number of workers simultameously ingesting data. Set to number of cores", "option", "J", int),
     reset_cache=("Do not use existing cache files (rebuild new ones)", "flag"),
+    transform_only=("Only run the preprocessing transform", "flag"),
+    transform_jobs=("Number of simultameous transform jobs", "option", "T", int),
     site=("Only run a specific site", "option", "S", str),
     masknan=("Mask nan rows instead of dropping them", "option", "M", float)
 )
-def main(input_path: str = '/project/lindner/moving/summer2018/Data_structure_3',
-         input_prefix: str = "Data_",
-         input_suffix: str = "",
+def main(ingest_path: str = '/project/lindner/moving/summer2018/Data_structure_3',
+         ingest_prefix: str = "Data_",
+         ingest_suffix: str = "",
          output_path: str = '/project/lindner/moving/summer2018/2019/data-formatted/parallel',
          num_jobs: int = 8,
          year_begin: int = 2000,
          year_end: int = 2018,
+         reset_cache: bool = False,
+         transform_only: bool = False,
+         transform_jobs: int = 1,
          site: str = None,
          masknan: float = None):
     workers = WorkerManager(num_jobs=num_jobs)
 
     site_index = 0
 
+    jobs = []
+
     for year_idx, year in enumerate(range(year_begin, year_end)):
 
-        input_name = "%s%d%s.csv" % (input_prefix, year, input_suffix)
-        transform_name = 'Transformed_' + input_name
+        input_name = "%s%d%s.csv" % (ingest_prefix, year, ingest_suffix)
+        cache_name = input_name + '.cache'
+
+        if not reset_cache:
+            if not os.path.exists(os.path.join(output_path, cache_name)):
+                print("Transform: %s" % input_name)
+                jobs.append(
+                    [year, masknan, os.path.join(ingest_path, input_name), os.path.join(output_path, cache_name)])
+        else:
+            jobs.append([year, masknan, os.path.join(ingest_path, input_name), os.path.join(output_path, cache_name)])
+
+    if len(jobs) > 0:
+        print("Starting Transform (files=%d, jobs=%d)" % (len(jobs), transform_jobs))
+
+    jobs_complete = 0
+
+    def async_cb(args):
+        nonlocal jobs_complete
+        jobs_complete += 1
+        print("Transform Progress: %d/%d" % (jobs_complete, len(jobs)))
+
+    transform_pool = multiprocessing.Pool(transform_jobs)
+
+    job_results = []
+    for job in jobs:
+        result = transform_pool.apply_async(transform_file, job, callback=async_cb)
+        job_results.append(result)
+
+    for res in job_results:
+        res.get()
+
+    transform_pool.close()
+    transform_pool.join()
+
+    if transform_only:
+        return
+
+    for year_idx, year in enumerate(range(year_begin, year_end)):
+
+        input_name = "%s%d%s.csv" % (ingest_prefix, year, ingest_suffix)
+        cache_name = input_name + '.cache'
 
         print("Processing: %s" % input_name)
 
-        df = pd.read_csv(os.path.join(input_path, transform_name))
+        df = pd.read_csv(os.path.join(output_path, cache_name))
 
         if site is None:
             sites = df['AQS_Code'].unique()
