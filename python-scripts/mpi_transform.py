bc29a5d0f7cfe6c5fae09a31bdb84db22ae275ea
Carroll Vance <csvance@uh.edu>
2019-03-30 11:48:02 -0500

    Adding l2 -> l3 and missing gps fill to mpi transform

--- python-scripts/mpi_transform.py
+++ python-scripts/mpi_transform.py
@@ -5,32 +5,59 @@ import plac
 import os
 import sys
 
-HOUSTON = ['48_201_0051', '48_201_0558', '48_201_0572',
-           '48_201_0551', '48_201_6000', '48_201_0669',
-           '48_201_0695', '48_201_0307', '48_201_0670',
-           '48_201_0673', '48_201_0671', '48_201_0069',
-           '48_201_1035', '48_201_0057', '48_201_1049',
-           '48_201_0803', '48_201_1034', '48_201_1052',
-           '48_201_0024']
+HOUSTON = {'48_201_0051': {'Longitude': -95.474167, 'Latitude': 29.623889},
+           '48_201_0558': {'Longitude': -95.3536111, 'Latitude': 29.5894444},
+           '48_201_0572': {'Longitude': -95.105, 'Latitude': 29.583333},
+           '48_201_0551': {'Longitude': -95.1602778, 'Latitude': 29.8586111},
+           '48_201_6000': {'Longitude': -95.2535982, 'Latitude': 29.6843603},
+           '48_201_0669': {'Longitude': -95.252778, 'Latitude': 29.694722},
+           '48_201_0695': {'Longitude': -95.3414, 'Latitude': 29.7176},
+           '48_201_0307': {'Longitude': -95.2599093, 'Latitude': 29.718799},
+           '48_201_0670': {'Longitude': -95.257222, 'Latitude': 29.701944},
+           '48_201_0673': {'Longitude': -95.256697, 'Latitude': 29.7023},
+           '48_201_0671': {'Longitude': -95.255, 'Latitude': 29.706111},
+           '48_201_0069': {'Longitude': -95.2611301, 'Latitude': 29.7062492},
+           '48_201_1035': {'Longitude': -95.2575931, 'Latitude': 29.7337263},
+           '48_201_0057': {'Longitude': -95.238469, 'Latitude': 29.734231},
+           '48_201_1049': {'Longitude': -95.2224669, 'Latitude': 29.716611},
+           '48_201_0803': {'Longitude': -95.1785379, 'Latitude': 29.7647877},
+           '48_201_1034': {'Longitude': -95.2205822, 'Latitude': 29.7679965},
+           '48_201_1052': {'Longitude': -95.38769, 'Latitude': 29.81453},
+           '48_201_0024': {'Longitude': -95.3261373, 'Latitude': 29.9010364}}
 
-def transform(df: pd.DataFrame, year: int, masknan: float = None, fillnan: float = None, sites = []) -> pd.DataFrame:
+def transform(df: pd.DataFrame, year: int, fillgps: bool = False, naninvalid: bool = False, dropnan: bool = False, masknan: float = None, fillnan: float = None, sites = []) -> pd.DataFrame:
 
     if len(sites) > 0:
-        df.drop(df[~df['AQS_Code'].isin(sites)].index, inplace=True)
+        df.drop(df[~df['AQS_Code'].isin(list(sites.keys()))].index, inplace=True)
 
-    if masknan is None and fillnan is None:
+    # This is probobly not needed anymore after changes Data_structure_3 (level3_data)
+    if naninvalid:
+        if year < 2014:
+            val = 'VAL'
+        if year >= 2014:
+            val = 'K'
 
+        df[df['nox_flag'] != val]['nox_flag'] = np.nan
+        df[df['no_flag'] != val]['no_flag'] = np.nan
+        df[df['no2_flag'] != val]['no2_flag'] = np.nan
+        df[df['o3_flag'] != val]['o3_flag'] = np.nan
+
+    # This is probobly not needed anymore after changes Data_structure_3 (level3_data)
+    if fillgps:
+        unique = df['AQS_Code'].unique()
+        for site in HOUSTON:
+            if site in unique:
+                df[df['AQS_Code'] == site]['Longitude'] = HOUSTON[site]['Longitude']
+                df[df['AQS_Code'] == site]['Latitude'] = HOUSTON[site]['Latitude']
+
+    if dropnan:
         if year < 2014:
             val = 'VAL'
         if year >= 2014:
             val = 'K'
 
         df.drop(df[df['nox_flag'] != val | df['no_flag'] != val | df['no2_flag'] != val | df['o3_flag'] != val | df['temp_flag'] != val].index, inplace=True)
-
-        df.drop(
-            ['co_flag', 'humid', 'humid_flag', 'pm25', 'pm25_flag', 'so2', 'so2_flag', 'solar', 'solar_flag', 'dew',
-             'dew_flag', 'redraw', 'co', 'no_flag', 'no2_flag', 'nox_flag', 'o3_flag', 'winddir_flag', 'windspd_flag',
-             'temp_flag', 'Longitude', 'Latitude'], inplace=True, axis=1)
+        df.drop(remove_cols, inplace=True, axis=1)
 
         df.dropna(inplace=True)
 
@@ -40,11 +67,6 @@ def transform(df: pd.DataFrame, year: int, masknan: float = None, fillnan: float
     df['day_of_year'] = pd.Series(pd.to_datetime(df['epoch'], unit='s'))
     df['day_of_year'] = df['day_of_year'].dt.dayofyear
 
-    if masknan is not None or fillnan is not None:
-        df.drop(
-            ['co_flag', 'humid', 'humid_flag', 'pm25', 'pm25_flag', 'so2', 'so2_flag', 'solar', 'solar_flag', 'dew',
-             'dew_flag', 'redraw', 'co', 'no_flag', 'no2_flag', 'nox_flag', 'o3_flag', 'winddir_flag', 'windspd_flag',
-             'temp_flag', 'Longitude', 'Latitude'], inplace=True, axis=1)
     if masknan is not None:
         df[df.isna()] = np.nan
     elif fillnan is not None:
@@ -54,17 +76,26 @@ def transform(df: pd.DataFrame, year: int, masknan: float = None, fillnan: float
 
 
 def run_job(job: dict):
-    chunk_idx = 0
 
-    for chunk in pd.read_csv(job['input_path'], chunksize=job['chunksize'], low_memory=False):
-        chunk = transform(chunk, job['year'], job['masknan'], job['fillnan'], job['sites'])
+    if job['cmd'] == 'transform':
 
-        if chunk_idx == 0:
-            chunk.to_csv(job['output_path'])
-        else:
-            chunk.to_csv(job['output_path'], mode='a', header=False)
+        chunk_idx = 0
 
-        chunk_idx += 1
+        for chunk in pd.read_csv(job['input_path'], chunksize=job['chunksize'], low_memory=False):
+            chunk = transform(chunk, year=job['year'],
+                              fillgps=job['fillgps'],
+                              naninvalid=job['naninvalid'],
+                              dropnan=job['dropnan'],
+                              masknan=job['masknan'],
+                              fillnan=job['fillnan'],
+                              sites=job['sites'])
+
+            if chunk_idx == 0:
+                chunk.to_csv(job['output_path'])
+            else:
+                chunk.to_csv(job['output_path'], mode='a', header=False)
+
+            chunk_idx += 1
 
 
 @plac.annotations(
@@ -74,20 +105,26 @@ def run_job(job: dict):
     output_path=("Path to write the resulting numpy sequences / transform cache", "option", "o", str),
     year_begin=("First year to process", "option", "b", int),
     year_end=("Year to stop with", "option", "e", int),
-    masknan=("Mask nan rows instead of dropping them", "option", "M", float),
-    fillnan=("Mask nan rows instead of dropping them", "option", "F", float),
-    houston=("Only run for Houston sites", "option", "H", bool),
+    fillgps=("Add correct GPS information because it is often missing in Data_structure_3", "flag", "G"),
+    naninvalid=("Set invalid col entries to nan", "flag", "N"),
+    dropnan=("Drop nan rows", "flag", "D"),
+    masknan=("Mask nan rows", "option", "M", float),
+    fillnan=("Fill nan rows", "option", "F", float),
+    houston=("Only run for Houston sites", "flag", "H"),
     chunksize=("Process this many records at one time", "option", 'C', int)
 )
-def main(input_path: str = '/project/lindner/moving/summer2018/Data_structure_3',
+def main(input_path: str = '/project/lindner/air-pollution/level3_data',
          input_prefix: str = "Data_",
          input_suffix: str = "",
-         output_path: str = '/project/lindner/moving/summer2018/2019/data-formatted/parallel',
+         output_path: str = '/project/lindner/air-pollution/current/2019/data-formatted/houston',
          year_begin: int = 2000,
          year_end: int = 2018,
+         fillgps: bool = False,
+         naninvalid: bool = False,
+         dropnan: bool = False,
          masknan: float = None,
          fillnan: float = None,
-         houston: bool = True,
+         houston: bool = False,
          chunksize: int = 200000):
 
     if masknan is not None and fillnan is not None:
@@ -110,8 +147,14 @@ def main(input_path: str = '/project/lindner/moving/summer2018/Data_structure_3'
             input_name = "%s%d%s.csv" % (input_prefix, year, input_suffix)
             transform_name = 'Transformed_' + input_name
 
-            job = {'cmd': 'transform', 'year': year, 'masknan': masknan,
-                   'fillnan': fillnan, 'sites': [],
+            job = {'cmd': 'transform',
+                   'year': year,
+                   'dropnan': dropnan,
+                   'fillgps': fillgps,
+                   'naninvalid': naninvalid,
+                   'masknan': masknan,
+                   'fillnan': fillnan,
+                   'sites': [],
                    'input_path': os.path.join(input_path, input_name),
                    'output_path': os.path.join(output_path, transform_name),
                    'chunksize': chunksize}
@@ -124,7 +167,7 @@ def main(input_path: str = '/project/lindner/moving/summer2018/Data_structure_3'
         outstanding_jobs = 0
         n_proc = 1
 
-        # Distribute optimizene full round robin of jobs
+        # Distribute one full round robin of jobs
         while len(jobs) > 0:
             comm.isend(jobs.pop(), dest=n_proc, tag=1)
 
